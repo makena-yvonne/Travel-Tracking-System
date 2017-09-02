998e124b76bf0f7cc8cee1fda7dad6ae6e2626b5
makena-yvonne <yvonnemakena3@gmail.com>
2017-09-02 16:36:27 +0300

    [bugged]added jquery validation

--- assets/js/travel_submit.js
+++ assets/js/travel_submit.js
@@ -1,4 +1,48 @@
 function travel_submit(){
+
+ alert("Details Submitted to Supervisor");
+	window.location = "index.html";
+}	
+
+ $(document).ready(function(){
+
+$('.submit').click(function(){
+    validateForm();   
+});
+
+function validateForm(){
+
+
+    var nameReg = /^[A-Za-z]+$/;
+    var numberReg =  /^[0-9]+$/;
+    
+    var names = $('#name').val();
+    var meeting = $('#meeting').val();
+  
+    // var message = $('#messageInput').val();
+
+    var inputVal = new Array(names, meeting);
+
+    var inputMessage = new Array("name", "meeting");
+
+    if ($('.error').hide()) {
+
+        if(inputVal[0] == ""){
+            $('#nameLabel').after('<span class="error"> Please enter your ' + inputMessage[0] + '</span>');
+        } 
+        else if(!nameReg.test(names)){
+            $('#nameLabel').after('<span class="error"> Letters only</span>');
+        }
+
+        if(inputVal[1] == ""){
+            $('#meetingLabel').after('<span class="error"> Please enter your ' + inputMessage[1] + '</span>');
+        }
+      
+}   
+
+else	
+{
 	alert("Details Submitted to Supervisor");
 	window.location = "index.html";
+}
 }
